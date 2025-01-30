15892e558edd4bb4a4f3aa8783cb9c20fddd12a8
csdavids519 <csdavds@proton.me>
2025-01-30 18:07:04 +0100

    created html templates and added url links to home page

--- sugar_rush/urls.py
+++ sugar_rush/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+    # path('checkout', include('checkout.urls')),
+    # path('products', include('products.urls')),
+    # path('profiles', include('profiles.urls')),
 ]
