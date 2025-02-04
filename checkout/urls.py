36170763760c431c236ca1910e033c927a317a2a
csdavids519 <csdavds@proton.me>
2025-02-04 15:25:55 +0100

    added add to basket function, checkout  view, and urls

--- checkout/urls.py
+++ checkout/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'checkout'
 
 urlpatterns = [
-    path('', views.checkout, name='checkout')
+    path('', views.checkout, name='checkout'),
+    path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
 ]
--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import (
+    render, redirect, reverse, HttpResponse, get_object_or_404
+    )
+from products.models import Product
+from django.contrib import messages
 
 # Create your views here.
 
@@ -7,3 +11,22 @@ def checkout(request):
     """ A view to return the index page """
 
     return render(request, 'checkout.html')
+
+
+def add_to_basket(request, item_id):
+    """ Add a quantity of the specified product to the shopping basket """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+
+    basket = request.session.get('basket', {})
+
+    if item_id in list(basket.keys()):
+        basket[item_id] += quantity
+    else:
+        basket[item_id] = quantity
+    
+    request.session['basket'] = basket
+    print(request.session['basket'])
+    return redirect(redirect_url)
+
