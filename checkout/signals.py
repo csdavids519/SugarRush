58b63554a1009f177d87619e7824a2a33aaeb8fc
csdavids519 <csdavds@proton.me>
2025-02-12 14:30:49 +0100

    combined signals.py in profiles app to create user and basket on user signup

--- checkout/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.contrib.auth.models import User
-from .models import Basket
-
-@receiver(post_save, sender=User)
-def create_user_basket(sender, instance, created, **kwargs):
-    if created:
-        Basket.objects.create(user=instance)
--- profiles/signals.py
+++ profiles/signals.py
@@ -2,9 +2,15 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Customer
+from checkout.models import Basket
 
 
 @receiver(post_save, sender=User)
 def create_customer_profile(sender, instance, created, **kwargs):
     if created:
-        Customer.objects.create(user=instance)
+        customer = Customer.objects.create(user=instance)
+        basket = Basket.objects.create(user=instance)
+
+        # Link basket to the customer
+        customer.basket_id = basket
+        customer.save()
