9fa31a55786777a2adbffd5b83f157dd5e1ded46
csdavids519 <csdavds@proton.me>
2025-02-20 12:17:46 +0100

    added Orders model to profiles

--- profiles/models.py
+++ profiles/models.py
@@ -1,20 +1,24 @@
 from django.db import models
 from products.models import Product
 from django.contrib.auth.models import User
-from checkout.models import Orders
+from checkout.models import Basket, ShippingInfo
 
 # Create your models here.
 
 
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='customer_username', null=True, blank=True)
-    order_id = models.ForeignKey(Orders, on_delete=models.DO_NOTHING,
-                                    null=True, blank=True)
-    basket_id = models.ForeignKey('checkout.Basket', on_delete=models.SET_NULL,
-                                    null=True, blank=True)
     first_name = models.CharField(max_length=254)
     last_name = models.CharField(max_length=254)
     email = models.EmailField(max_length=254)
 
     def __str__(self):
         return f"Customer {self.user}"
+
+
+class Orders(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='order_user', null=True, blank=True)
+    basket_order = models.ForeignKey(Basket, on_delete=models.SET_NULL,
+                                    null=True, blank=True)
+    shipping_order = models.ForeignKey(ShippingInfo, on_delete=models.SET_NULL,
+                                    null=True, blank=True)
