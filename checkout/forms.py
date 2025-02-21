6f7e965a4af27aff5d6e6cab83c6246a68ef5e50
csdavids519 <csdavds@proton.me>
2025-02-21 22:40:52 +0100

    add style to shipping form and shipping info page

--- checkout/forms.py
+++ checkout/forms.py
@@ -3,13 +3,36 @@ from .models import ShippingInfo
 
 
 class OrderForm(forms.ModelForm):
-    phone_number = forms.CharField(required=False)
-    street_address2 = forms.CharField(required=False)
-    postcode = forms.CharField(required=True)
-
     class Meta:
         model = ShippingInfo
         fields = ('full_name', 'email', 'phone_number',
                   'street_address1', 'street_address2',
                   'town_or_city', 'postcode', 'country',
                   'state',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name*',
+            'email': 'Email Address*',
+            'phone_number': 'Phone Number',
+            'postcode': 'Postal Code*',
+            'town_or_city': 'Town or City*',
+            'street_address1': 'Street Address 1*',
+            'street_address2': 'Street Address 2',
+            'state': 'County, State or Locality*',
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            self.fields[field].label = False
+            if field != 'street_address2':
+                if field != 'phone_number':
+                    self.fields[field].widget.attrs['required'] = True
+            if field != 'country':
+                placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
