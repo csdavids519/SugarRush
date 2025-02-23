8abe3400550e37f8874ad09371fa997c1c5bfb35
csdavids519 <csdavds@proton.me>
2025-02-23 16:31:04 +0100

    added flavor and type choice to product model updated products fixtures

--- products/models.py
+++ products/models.py
@@ -4,13 +4,26 @@ from django.db import models
 
 
 class Product(models.Model):
+    FLAVOR = (
+        ('Sour', 'Sour'),
+        ('Sweet', 'Sweet'),
+        ('Fruity', 'Fruity'),
+    )
+
+    TYPE = (
+        ('Sticky', 'Sticky'),
+        ('Hard', 'Hard'),
+        ('Gummy', 'Gummy'),
+        ('Chocolate', 'Chocolate'),
+    )
+
     product_id = models.IntegerField(unique=True, null=False)
     name = models.CharField(max_length=254)
     product_image = models.ImageField(null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, null=False)
     description = models.TextField(null=True, blank=True)
-    flavor = models.CharField(max_length=254,blank=True, null=True)
-    type = models.CharField(max_length=254,blank=True, null=True)
+    flavor = models.CharField(max_length=254,blank=True, null=True, choices=FLAVOR)
+    type = models.CharField(max_length=254,blank=True, null=True, choices=TYPE)
 
     def __str__(self):
         return str(self.product_id)
