98e3a7a04439038bff606e18da09972752966782
csdavids519 <csdavds@proton.me>
2025-02-24 13:53:13 +0100

    add promo products to index page, added images

--- home/views.py
+++ home/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import (render, get_object_or_404)
+from products.models import Product
 
 
 def index(request):
     """ A view to return the index page """
+    promo_new = get_object_or_404(Product, pk=1)
+    promo_popular_1 = get_object_or_404(Product, pk=4)
+    promo_popular_2 = get_object_or_404(Product, pk=8)
+    promo_popular_3 = get_object_or_404(Product, pk=6)
 
-    return render(request, 'index.html')
+    context = {
+        'promo_new': promo_new,
+        'promo_popular_1': promo_popular_1,
+        'promo_popular_2': promo_popular_2,
+        'promo_popular_3': promo_popular_3,
+    }
+
+    return render(request, 'index.html', context)
