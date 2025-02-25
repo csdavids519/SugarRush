84734c4d4431c37cee6f5c99768de5aa7c4379f8
csdavids519 <csdavds@proton.me>
2025-02-25 13:47:46 +0100

    fixed - cart total display issue

--- checkout/contexts.py
+++ checkout/contexts.py
@@ -8,19 +8,12 @@ def update_basket_total(request):
         user = request.user
 
         # update basket pice total
+        # find current basket
         basket = Basket.objects.filter(user=user).last()
-
+        # get all products in current basket
         basket_products = basket.basket_products.all()
-        print('UPDATE: BASKET FOUND')
-        print(f"Basket user: {basket.user}")
-        print(f"Basket ID: {basket.id}")
-        # total_price = basket.basket_products.aggregate(
-        #     total=Sum(F('product__price') * F('quantity'))
-        #     )['total']
-        # print(f"total_price: {total_price}")
+        # sum total cost
         total_price = sum(i.product.price * i.quantity for i in basket_products)
-        print('UPDATE: TOTAL')
-        print(f"total price: {total_price}")
 
         return {'grand_total': total_price}
     return {'grand_total': 0.00}
