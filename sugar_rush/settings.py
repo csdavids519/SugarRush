478d3b34c0221ccb057a6fda7d2069f1c062df11
csdavids519 <csdavds@proton.me>
2025-02-27 13:27:33 +0100

    added Heroku Procfile, runtime txt, added heroku to allowed hosts, installed gunicorn

--- sugar_rush/settings.py
+++ sugar_rush/settings.py
@@ -38,9 +38,15 @@ STRIPE_WEBHOOK_SECRET = ''
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['http://localhost:8000']
+ALLOWED_HOSTS = ['localhost',
+                 '127.0.0.1',
+                 'sugar-rush-7a055e26dddb.herokuapp.com/',
+                 ]
 
+
+CSRF_TRUSTED_ORIGINS = ['localhost',
+                        '127.0.0.1',
+                        ]
 
 # Application definition
 
@@ -137,16 +143,20 @@ WSGI_APPLICATION = 'sugar_rush.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
-# DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-# }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
+
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
