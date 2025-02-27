7aa6b1b6a10417e57856962687612ff7fc9e3d65
csdavids519 <csdavds@proton.me>
2025-02-27 14:53:03 +0100

    add images and collectstatic for whitenoise host to heroku

---
+++ staticfiles/js/checkout.js
@@ -0,0 +1,22 @@
+// This is your test secret API key.
+const stripe = Stripe("pk_test_51QsjeH02ahKmoBWWiupTVTSOypH3073b25gWzF4vB0vk9SIvAWpCFPVkE5Dp5P2R6eNmvBRevBxR07Xzyv1QHV6s00IWGTX1Cx");
+
+initialize();
+
+// Create a Checkout Session
+async function initialize() {
+    const fetchClientSecret = async () => {
+        const response = await fetch("/checkout/create-checkout-session/", {
+            method: "POST",
+        });
+        const { clientSecret } = await response.json();
+        return clientSecret;
+    };
+
+    const checkout = await stripe.initEmbeddedCheckout({
+        fetchClientSecret,
+    });
+
+    // Mount Checkout
+    checkout.mount('#checkout');
+}
