14558dc2c51b2ab5e4d10e166a34e923fa29bcd7
csdavids519 <csdavids@proton.me>
2025-06-18 19:56:59 +0200

    added new views to manage reviews

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from profiles.models import Customer
+from .models import Review
+from .forms import ReviewForm
 
 
 def profiles(request):
@@ -15,3 +17,40 @@ def order_history(request):
     except Customer.DoesNotExist:
         return render(request, 'order_history.html', {'order_results': None})
     return render(request, 'order_history.html', {'order_results': orders})
+
+
+def review_list(request):
+    """ view to create list of current user reviews """
+    reviews = Review.objects.all()
+    return render(request, 'review_list.html', {'reviews': reviews})
+
+
+def review_create(request):
+    """ view to create a new review """
+    form = ReviewForm(request.POST or None)
+    if form.is_valid():
+        review = form.save(commit=False)
+        review.user = request.user
+        review.save()
+        return redirect('profiles:review_list')
+    return render(request, 'review_form.html', {'form': form})
+
+
+def review_update(request, pk):
+    """ view to edit an existing review """
+    review = get_object_or_404(Review, pk=pk, user=request.user)
+    form = ReviewForm(request.POST or None, instance=review)
+    print("Review:", review.pk)
+    if form.is_valid():
+        form.save()
+        return redirect('profiles:review_list')
+    return render(request, 'review_form.html', {'form': form})
+
+
+def review_delete(request, pk):
+    """ view to delete a reveiw """
+    review = get_object_or_404(Review, pk=pk, user=request.user)
+    if request.method == 'POST':
+        review.delete()
+        return redirect('profiles:review_list')
+    return render(request, 'review_confirm_delete.html', {'review': review})
