f5631dadb86bf2980f7a3ca6cf7366f7467bd125
csdavids519 <csdavids@proton.me>
2025-06-18 19:57:45 +0200

    pass reviews model to index page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import (render, get_object_or_404)
 from products.models import Product
+from profiles.models import Review
 
 
 def index(request):
@@ -8,12 +9,14 @@ def index(request):
     promo_popular_1 = get_object_or_404(Product, pk=4)
     promo_popular_2 = get_object_or_404(Product, pk=8)
     promo_popular_3 = get_object_or_404(Product, pk=6)
+    reviews = Review.objects.all().order_by('-updated')
 
     context = {
         'promo_new': promo_new,
         'promo_popular_1': promo_popular_1,
         'promo_popular_2': promo_popular_2,
         'promo_popular_3': promo_popular_3,
+        'reviews': reviews,
     }
 
     return render(request, 'index.html', context)
