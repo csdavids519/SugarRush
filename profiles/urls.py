7f582dbccfff25bd405182a4d5eb6f4b211f6fd1
csdavids519 <csdavids@proton.me>
2025-06-18 19:58:15 +0200

    update profile urls for review pages

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,4 +5,8 @@ app_name = 'profiles'
 
 urlpatterns = [
     path('', views.profiles, name='profiles'),
+    path('reviews', views.review_list, name='review_list'),
+    path('create/', views.review_create, name='review_create'),
+    path('edit/<int:pk>/', views.review_update, name='review_update'),
+    path('delete/<int:pk>/', views.review_delete, name='review_delete'),
 ]
