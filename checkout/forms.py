f7f29a2d7b3d065082d4b75c0327ca2215fff085
csdavids519 <csdavids@proton.me>
2025-06-25 14:48:55 +0200

    change shipping form to order form to match boutique ado example

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,40 +1,39 @@
 from django import forms
-from .models import ShippingInfo
+from .models import Order
 
 
-class ShippingForm(forms.ModelForm):
+class OrderForm(forms.ModelForm):
     class Meta:
-        model = ShippingInfo
-        exclude = ('user',)
+        model = Order
         fields = ('full_name', 'email', 'phone_number',
                   'street_address1', 'street_address2',
                   'town_or_city', 'postcode', 'country',
-                  'state',)
+                  'county',)
 
     def __init__(self, *args, **kwargs):
         """
-        Code Reference Boutique_Ado
         Add placeholders and classes, remove auto-generated
         labels and set autofocus on first field
         """
         super().__init__(*args, **kwargs)
         placeholders = {
-            'full_name': 'Full Name*',
-            'email': 'Email Address*',
+            'full_name': 'Full Name',
+            'email': 'Email Address',
             'phone_number': 'Phone Number',
-            'postcode': 'Postal Code*',
-            'town_or_city': 'Town or City*',
-            'street_address1': 'Street Address 1*',
+            'country': 'Country',
+            'postcode': 'Postal Code',
+            'town_or_city': 'Town or City',
+            'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'state': 'County, State or Locality*',
+            'county': 'County',
         }
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            self.fields[field].label = False
-            if field != 'street_address2':
-                if field != 'phone_number':
-                    self.fields[field].widget.attrs['required'] = True
-            if field != 'country':
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
                 placeholder = placeholders[field]
-                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
